2b57ce9900bfe7194acf30a5fd284ca110b3f458
crystaljoy.tolentino@gmail.com <crystaljoy.tolentino@gmail.com>
2019-04-12 11:46:11 +0800

    added front

---
+++ front/src/app/components/create/create.component.ts
@@ -0,0 +1,34 @@
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { IssueService } from '../../services/issue.service';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-create',
+  templateUrl: './create.component.html',
+  styleUrls: ['./create.component.css']
+})
+export class CreateComponent implements OnInit {
+
+  createForm: FormGroup;
+
+  constructor(private issueService: IssueService, private fb: FormBuilder, private router: Router) {
+    this.createForm = this.fb.group({
+      title: ['', Validators.required],
+      responsible: '',
+      description: '',
+      severity: ''
+    });
+  }
+
+  addIssue(title, responsible, description, severity) {
+    this.issueService.addIssue(title, responsible, description, severity).subscribe(() => {
+      this.issueService.getIssues();
+      this.router.navigate(['/list']);
+    });
+  }
+
+  ngOnInit() {
+  }
+
+}
